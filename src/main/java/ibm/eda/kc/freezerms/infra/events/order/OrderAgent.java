a9c34cd62082f816737fcbfab88ae49e07cb2638
Eugen Gerb <egb05@gmx.de>
2022-11-10 23:38:32 +0100

    add working manual opentelemetry instrumentation

--- src/main/java/ibm/eda/kc/freezerms/infra/events/order/OrderAgent.java
+++ src/main/java/ibm/eda/kc/freezerms/infra/events/order/OrderAgent.java
@@ -1,12 +1,16 @@
 package ibm.eda.kc.freezerms.infra.events.order;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+import io.smallrye.reactive.messaging.TracingMetadata;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
 
@@ -33,19 +37,26 @@ public class OrderAgent {
     ReeferEventProducer reeferEventProducer;
 
     @Incoming("orders")
-    public CompletionStage<Void> processOrder(Message<OrderEvent> messageWithOrderEvent){
+    public CompletionStage<Void> processOrder(Message<OrderEvent> messageWithOrderEvent) {
         logger.info("Received order : " + messageWithOrderEvent.getPayload().orderID);
         OrderEvent oe = messageWithOrderEvent.getPayload();
-        switch( oe.getType()){
-            case OrderEvent.ORDER_CREATED_TYPE:
-                processOrderCreatedEvent(oe);
-                break;
-            case OrderEvent.ORDER_UPDATED_TYPE:
-                logger.info("Receive order update " + oe.status);
-                compensateOrder(oe.orderID);
-                break;
-            default:
-                break;
+        Optional<TracingMetadata> optionalTracingMetadata = TracingMetadata.fromMessage(messageWithOrderEvent);
+        if (optionalTracingMetadata.isPresent()) {
+            TracingMetadata tracingMetadata = optionalTracingMetadata.get();
+            try (Scope scope = tracingMetadata.getCurrentContext().makeCurrent()) {
+                logger.info("TraceId " + Span.current().getSpanContext().getTraceId());
+                switch (oe.getType()) {
+                    case OrderEvent.ORDER_CREATED_TYPE:
+                        processOrderCreatedEvent(oe);
+                        break;
+                    case OrderEvent.ORDER_UPDATED_TYPE:
+                        logger.info("Receive order update " + oe.status);
+                        compensateOrder(oe.orderID);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
         return messageWithOrderEvent.ack();
     }
@@ -54,21 +65,21 @@ public class OrderAgent {
      * When order created, search for reefers close to the pickup location,
      * add them in the container ids and send an event as ReeferAllocated
      */
-    public ReeferEvent processOrderCreatedEvent( OrderEvent oe){
-        OrderCreatedEvent oce = (OrderCreatedEvent)oe.payload;
-        List<Reefer> reefers = repo.getReefersForOrder(oe.orderID, 
-                                oce.pickupCity, 
-                                oe.quantity);
-        if (reefers.size() > 0) {
-            ReeferAllocated reeferAllocatedEvent = new ReeferAllocated(reefers,oe.orderID);
-            ReeferEvent re = new ReeferEvent(ReeferEvent.REEFER_ALLOCATED_TYPE,reeferAllocatedEvent);
-            re.reeferID = reeferAllocatedEvent.reeferIDs;     
-            reeferEventProducer.sendEvent(re.reeferID,re);  
+    public ReeferEvent processOrderCreatedEvent(OrderEvent oe) {
+        OrderCreatedEvent oce = (OrderCreatedEvent) oe.payload;
+        List<Reefer> reefers = repo.getReefersForOrder(oe.orderID,
+                oce.pickupCity,
+                oe.quantity);
+        if (!reefers.isEmpty()) {
+            ReeferAllocated reeferAllocatedEvent = new ReeferAllocated(reefers, oe.orderID);
+            ReeferEvent re = new ReeferEvent(ReeferEvent.REEFER_ALLOCATED_TYPE, reeferAllocatedEvent);
+            re.reeferID = reeferAllocatedEvent.reeferIDs;
+            reeferEventProducer.sendEvent(re.reeferID, re);
             return re;
         }
         return null;
     }
- 
+
     public void compensateOrder(String oid) {
         repo.cleanTransaction(oid);
     }
--- src/main/java/ibm/eda/kc/freezerms/infra/events/reefer/ReeferEventProducer.java
+++ src/main/java/ibm/eda/kc/freezerms/infra/events/reefer/ReeferEventProducer.java
@@ -5,6 +5,8 @@ import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import io.opentelemetry.context.Context;
+import io.smallrye.reactive.messaging.TracingMetadata;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.eclipse.microprofile.reactive.messaging.Message;
@@ -13,22 +15,24 @@ import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
 
 @ApplicationScoped
 public class ReeferEventProducer {
-	Logger logger = Logger.getLogger(ReeferEventProducer.class.getName());
+    Logger logger = Logger.getLogger(ReeferEventProducer.class.getName());
     @Channel("reefers")
-	public Emitter<ReeferEvent> reeferEventProducer;
+    public Emitter<ReeferEvent> reeferEventProducer;
 
 
-    public void sendEvent(String key, ReeferEvent reeferEvent){
-		logger.info("Send event -> " + reeferEvent.reeferID + " type of " + reeferEvent.getType() +  " ts:" + reeferEvent.getTimestampMillis());
-		reeferEventProducer.send(Message.of(reeferEvent).addMetadata(OutgoingKafkaRecordMetadata.<String>builder()
-			.withKey(key).build())
-			.withAck( () -> {
-				
-				return CompletableFuture.completedFuture(null);
-			})
-			.withNack( throwable -> {
-				return CompletableFuture.completedFuture(null);
-			}));
-	}
+    public void sendEvent(String key, ReeferEvent reeferEvent) {
+        logger.info("Send event -> " + reeferEvent.reeferID + " type of " + reeferEvent.getType() + " ts:" + reeferEvent.getTimestampMillis());
+        reeferEventProducer.send(Message.of(reeferEvent)
+                .addMetadata(OutgoingKafkaRecordMetadata.<String>builder().withKey(key).build())
+                .withAck(() -> {
+
+                    return CompletableFuture.completedFuture(null);
+                })
+                .withNack(throwable -> {
+                    return CompletableFuture.completedFuture(null);
+                })
+                .addMetadata(TracingMetadata.withCurrent(Context.current()))
+        );
+    }
 
 }
